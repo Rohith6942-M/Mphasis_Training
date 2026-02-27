ba1cafbea09a014b74d85bd0d2ea1dce3a0480ce
Rohith6942-M <rohith2004m@gmail.com>
2026-02-27 16:17:28 +0530

    15th push commit

---
+++ SpringBootMVCExp2-1/src/main/java/com/test/SpringBootMvcExp21Application.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcExp21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcExp21Application.class, args);
+	}
+
+}
